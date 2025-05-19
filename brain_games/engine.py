61249f0a63bcf0d34a53d6ef3afc22d118280973
Konstantin <konstantin.zabolotnev@gmail.com>
2025-05-19 21:56:30 +0300

    Fix SonarCube errors

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -13,7 +13,8 @@ def run_engine(description, generate_round):
         user_answer = input("Your answer: ").strip()
         
         if user_answer != correct_answer:
-            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"'{user_answer}' is wrong answer ;(.")
+            print(f"Correct answer was '{correct_answer}'.")
             print(f"Let's try again, {name}!")
             return
         else:
