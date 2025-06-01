fea42ba62990ef518b39d937254e47540f039853
Konstantin <konstantin.zabolotnev@gmail.com>
2025-06-01 20:35:36 +0300

    Added comments # NOSONAR

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -8,7 +8,7 @@ DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def generate_round():
-    number = random.randint(1, 100)
+    number = random.randint(1, 100) # NOSONAR
     correct_answer = 'yes' if number % 2 == 0 else 'no'
     return str(number), correct_answer
 
